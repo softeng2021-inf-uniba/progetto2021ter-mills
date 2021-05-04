b407c8c3ef5bd2524ab026dd177bf1c33fefb7f8
Giuliano Picilli <giulianopicilli@hevolus.it>
2021-05-04 20:49:23 +0200

    Commentata interfaccia ObserverStatus

--- src/main/java/it/uniba/main/gioco/ObserverStatus.java
+++ src/main/java/it/uniba/main/gioco/ObserverStatus.java
@@ -2,6 +2,11 @@ package it.uniba.main.gioco;
 
 import it.uniba.main.utilities.Observer;
 
+
+/**
+ * <<noECB>>
+ * interfaccia che eredita da Observer con argomento di tipo Status
+ */
 public interface ObserverStatus extends Observer<Status>
 {
     @Override
