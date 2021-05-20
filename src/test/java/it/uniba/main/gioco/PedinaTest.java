b525383182e0c2e232628a8e1687bfd0f61b6581
Fabio Spaccavento <f.spaccavento3@studenti.uniba.it>
2021-05-20 19:30:44 +0200

    Eseguito test classe 'Pedina'

---
+++ src/test/java/it/uniba/main/gioco/PedinaTest.java
@@ -0,0 +1,75 @@
+package it.uniba.main.gioco;
+
+import it.uniba.main.gioco.damiera.Pedina;
+import it.uniba.main.gioco.damiera.TipoPedina;
+import it.uniba.main.utilities.Posizione;
+import it.uniba.main.utilities.Strings;
+import org.junit.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PedinaTest
+{
+    private Pedina pedinaBianca;
+    private Pedina pedinaNera;
+
+    @Before
+    public void setUpPedina()
+    {
+        pedinaBianca = new Pedina(TipoPedina.bianca, new Posizione(7,7));
+        pedinaNera = new Pedina(TipoPedina.nera, new Posizione(0,0));
+    }
+
+    @Test
+    public void testGetTipoPedinaBianca()
+    {
+        assertEquals(pedinaBianca.getTipo(), TipoPedina.bianca);
+    }
+
+    @Test
+    public void testGetTipoPedinaNera()
+    {
+        assertEquals(pedinaNera.getTipo(), TipoPedina.nera);
+    }
+
+    @Test
+    public void testGetDirezionePedinaBianca()
+    {
+        int direzione = -1;
+        assertEquals(pedinaBianca.getDirezione(), direzione);
+    }
+
+    @Test
+    public void testGetDirezionePedinaNera()
+    {
+        int direzione = 1;
+        assertEquals(pedinaNera.getDirezione(), direzione);
+    }
+
+    @Test
+    public void testToStringPedinaBianca()
+    {
+        assertEquals(pedinaBianca.toString(), Strings.PEDINA_BIANCA);
+    }
+
+    @Test
+    public void testToStringPedinaNera()
+    {
+        assertEquals(pedinaNera.toString(), Strings.PEDINA_NERA);
+    }
+
+    @Test
+    public void testToStringPedinaBiancaRegina()
+    {
+        pedinaBianca.isDama = true;
+        assertEquals(pedinaBianca.toString(), Strings.PEDINA_REGINA_BIANCA);
+    }
+
+    @Test
+    public void testToStringPedinaNeraRegina()
+    {
+        pedinaNera.isDama = true;
+        assertEquals(pedinaNera.toString(), Strings.PEDINA_REGINA_NERA);
+    }
+
+}
