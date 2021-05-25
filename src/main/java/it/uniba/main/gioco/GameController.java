7268261c2714ae98bd89293afdded243b804adb4
danielesaccà <d.sacca@studenti.uniba.it>
2021-05-25 20:10:21 +0200

    risolti errori checkstyle classe GameController

--- src/main/java/it/uniba/main/gioco/GameController.java
+++ src/main/java/it/uniba/main/gioco/GameController.java
@@ -10,8 +10,7 @@ import it.uniba.main.utilities.Utilities;
  * <<Boundary>>
  * Classe che gestisce l'interazione con l'utente e mostra i messaggi
  */
-public class GameController
-{
+public class GameController {
     private GameModel gameModel;
 
     private Observer<Status> observerStatus = status -> stampaNuovoStato(status);
@@ -19,17 +18,15 @@ public class GameController
 
     private boolean uscitaRichiesta = false;
 
-    public GameController(GameModel gameModel)
-    {
-        this.gameModel = gameModel;
+    public GameController(final GameModel model) {
+        this.gameModel = model;
 
         gameModel.getOnStatusChanged().register(observerStatus);
         gameModel.getOnMessagesCalled().register(observerMessages);
 
         Comando cmd = null;
 
-        while (!uscitaRichiesta)
-        {
+        while (!uscitaRichiesta) {
             String str = Utilities.getStringaDaTastiera();
             str = Utilities.pulisciStringa(str);
 
@@ -40,68 +37,51 @@ public class GameController
 
     }
 
-    private void avviaNuovaPartita()
-    {
+    private void avviaNuovaPartita() {
         gameModel.startGame();
     }
 
-    private void gestioneAbbandona()
-    {
+    private void gestioneAbbandona() {
         Boolean risultato = null;
         System.out.println(Strings.CONFERMA_ABBANDONO);
-        while (risultato == null)
-        {
+        while (risultato == null) {
             String conferma = Utilities.getStringaDaTastiera();
             conferma = Utilities.pulisciStringa(conferma);
-            if (conferma.equalsIgnoreCase("si"))
-            {
+            if (conferma.equalsIgnoreCase("si")) {
                 System.out.println(Strings.PARTITA_ABBANDONATA);
                 gameModel.abbandonaPartita();
                 risultato = true;
-            }
-            else if (conferma.equalsIgnoreCase("no"))
-            {
+            } else if (conferma.equalsIgnoreCase("no")) {
                 System.out.println(Strings.PARTITA_NON_ABBANDONATA);
                 risultato = false;
-            }
-            else
-            {
+            } else {
                 System.out.println(Strings.RISPOSTA_ERRATA);
             }
         }
     }
 
-    private void stampaDamiera()
-    {
+    private void stampaDamiera() {
         System.out.println(gameModel.getDamiera());
     }
 
-    private void stampaNumeri()
-    {
-        int DIM = gameModel.getDimDamiera();
+    private void stampaNumeri() {
+        int dim = gameModel.getDimDamiera();
         StringBuffer stringa = new StringBuffer();
         int contatore = 1;
-        int numCaselleNere = (int) Math.floor(Math.log10((DIM * DIM) / 2.0));
-        for (int i = 0; i < DIM; i++)
-        {
-            for (int j = 0; j < DIM; j++)
-            {
-                if (((i * (DIM + 1) + j) % 2) == 0)
-                {
+        int numCaselleNere = (int) Math.floor(Math.log10((dim * dim) / 2.0));
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (((i * (dim + 1) + j) % 2) == 0) {
                     stringa.append(Strings.RGB_INDACO_BG + Strings.ANSI_BLACK_FG + contatore);
 
-                    for (int k = 0; k < numCaselleNere - Math.floor(Math.log10(contatore)); k++)
-                    {
+                    for (int k = 0; k < numCaselleNere - Math.floor(Math.log10(contatore)); k++) {
                         stringa.append(Strings.RGB_INDACO_BG + Strings.RGB_INDACO_FG + "_");
                     }
                     stringa.append(" ");
                     contatore++;
-                }
-                else
-                {
+                } else {
                     stringa.append(Strings.RGB_CREMA_BG);
-                    for (int k = 0; k <= numCaselleNere; k++)
-                    {
+                    for (int k = 0; k <= numCaselleNere; k++) {
                         stringa.append(Strings.RGB_CREMA_BG + Strings.RGB_CREMA_FG + "_");
                     }
                     stringa.append(" ");
@@ -111,90 +91,55 @@ public class GameController
         }
         System.out.println(stringa.toString());
     }
-
-
-    void presa(String arg)
-    {
-        String caselle[] = arg.split("x");
+     private void presa(final String arg) {
+        String[] caselle = arg.split("x");
         gameModel.eseguiPresa(caselle);
     }
 
-    void spostamentoSemplice(String arg)
-    {
-        String caselle[] = arg.split("-");
+
+    private void spostamentoSemplice(final String arg) {
+        String[] caselle = arg.split("-");
         gameModel.eseguiSpostamentoSemplice(Integer.parseInt(caselle[0]), Integer.parseInt(caselle[1]));
     }
-
-    private void comandiInGioco(Comando cmd)
-    {
-        if (cmd == Comando.help)
-        {
+    private void comandiInGioco(final Comando cmd) {
+        if (cmd == Comando.help) {
             System.out.println(Strings.HELP_MSG);
-        }
-        else if (cmd == Comando.abbandona)
-        {
+        } else if (cmd == Comando.abbandona) {
             gestioneAbbandona();
-        }
-        else if (cmd == Comando.damiera)
-        {
+        } else if (cmd == Comando.damiera) {
             stampaDamiera();
-        }
-        else if (cmd == Comando.numeri)
-        {
+        } else if (cmd == Comando.numeri) {
             stampaNumeri();
-        }
-        else if (cmd == Comando.tempo)
-        {
+        } else if (cmd == Comando.tempo) {
             stampaTempoGiocatori();
-        }
-        else if (cmd == Comando.spostamentoSemplice)
-        {
+        } else if (cmd == Comando.spostamentoSemplice) {
             spostamentoSemplice(cmd.getInputStr());
-        }
-        else if (cmd == Comando.presa)
-        {
+        } else if (cmd == Comando.presa) {
             presa(cmd.getInputStr());
-        }
-        else if (cmd == Comando.prese)
-        {
+        } else if (cmd == Comando.prese) {
             stampaPrese();
-        }
-        else if (cmd == Comando.mosse)
-        {
+        } else if (cmd == Comando.mosse) {
             stampaStoricoMosse();
-        }
-        else
-        {
+        } else {
             System.out.println(Strings.ERRORE_COMANDO_FUORI_GIOCO);
         }
     }
 
-    private void comandiFuoriGioco(Comando cmd)
-    {
-        if (cmd == Comando.help)
-        {
+    private void comandiFuoriGioco(final Comando cmd) {
+        if (cmd == Comando.help) {
             System.out.println(Strings.HELP_MSG);
-        }
-        else if (cmd == Comando.gioca)
-        {
+        } else if (cmd == Comando.gioca) {
             avviaNuovaPartita();
-        }
-        else if (cmd == Comando.numeri)
-        {
+        } else if (cmd == Comando.numeri) {
             stampaNumeri();
-        }
-        else if (cmd == Comando.esci)
-        {
+        } else if (cmd == Comando.esci) {
             esciDalGioco();
-        }
-        else
-        {
+        } else {
             System.out.println(Strings.ERRORE_COMANDO_IN_GIOCO);
         }
     }
 
-    private void stampaTempoGiocatori()
-    {
+    private void stampaTempoGiocatori() {
         long tempoBianco = gameModel.getCronometroBianco().getTempoTrascorsoMillis();
         long tempoNero = gameModel.getCronometroNero().getTempoTrascorsoMillis();
         String tempo = "Tempo " + Strings.GIOCATORE_BIANCO + " " + Utilities.getStringaTempo(tempoBianco);
@@ -202,85 +147,68 @@ public class GameController
         System.out.println(tempo);
     }
 
-    private void esciDalGioco()
-    {
+    private void esciDalGioco() {
         Boolean risultato = null;
 
         System.out.println(Strings.CONFERMA_USCITA);
-        while (risultato == null)
-        {
+        while (risultato == null) {
             String conferma = Utilities.getStringaDaTastiera();
             conferma = Utilities.pulisciStringa(conferma);
-            if (conferma.equalsIgnoreCase("si"))
-            {
+            if (conferma.equalsIgnoreCase("si")) {
                 risultato = true;
-            }
-            else if (conferma.equalsIgnoreCase("no"))
-            {
+            } else if (conferma.equalsIgnoreCase("no")) {
                 System.out.println(Strings.USCITA_NON_ESEGUITA);
                 risultato = false;
-            }
-            else
-            {
+            } else {
                 System.out.println(Strings.RISPOSTA_ERRATA);
             }
         }
         uscitaRichiesta = risultato;
     }
 
-    private void stampaNuovoStato(Status status)
-    {
+    private void stampaNuovoStato(final Status status) {
         System.out.println(status.getMsg());
     }
 
-    private void stampaNuovoMessaggio(Messaggio msg)
-    {
+    private void stampaNuovoMessaggio(final Messaggio msg) {
         System.out.println(msg.getMsg());
     }
 
-    private void controlloComando(Comando cmd)
-    {
-        if (cmd == null)
-        {
+    private void controlloComando(final Comando cmd) {
+        if (cmd == null) {
             System.out.println(Strings.COMANDO_ERRATO);
-        }
-        else if (gameModel.getIsPlaying())
-        {
+        } else if (gameModel.getIsPlaying()) {
             comandiInGioco(cmd);
-        }
-        else
-        {
+        } else {
             comandiFuoriGioco(cmd);
         }
 
     }
 
-    public void stampaPrese()
-    {
+    /**
+     *
+     */
+     public void stampaPrese() {
         StringBuffer result = new StringBuffer();
         result.append(Strings.PRESE_MSG + Strings.GIOCATORE_BIANCO + ": ");
-        for (int i = 0; i < gameModel.getPunteggioBianco(); i++)
-        {
+        for (int i = 0; i < gameModel.getPunteggioBianco(); i++) {
             result.append(Strings.PEDINA_NERA);
         }
         result.append("\n" + Strings.PRESE_MSG + Strings.GIOCATORE_NERO + ": ");
-        for (int i = 0; i < gameModel.getPunteggioNero(); i++)
-        {
+        for (int i = 0; i < gameModel.getPunteggioNero(); i++) {
             result.append(Strings.PEDINA_BIANCA);
         }
         System.out.println(result.toString());
     }
 
-    public void stampaStoricoMosse()
-    {
-        if (gameModel.getStoricoMosse().size() <= 0)
-        {
+    /**
+     *
+     */
+    public void stampaStoricoMosse() {
+        if (gameModel.getStoricoMosse().size() <= 0) {
             System.out.println(Strings.NESSUNA_MOSSA);
-        }
-        else
-        {
-            for (int i = 0; i < gameModel.getStoricoMosse().size(); i++)
-            {
+        } else {
+            for (int i = 0; i < gameModel.getStoricoMosse().size(); i++) {
                 System.out.println(gameModel.getStoricoMosse().get(i));
             }
         }
