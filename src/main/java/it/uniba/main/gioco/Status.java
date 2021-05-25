d37f05495be7568b0befafce6aa6e4100e1a1183
GiorgioGrimaldi <g.grimaldi23@studenti.uniba.it>
2021-05-25 18:44:05 +0200

    Aggiunti commenti javadoc nella classe Status

--- src/main/java/it/uniba/main/gioco/Status.java
+++ src/main/java/it/uniba/main/gioco/Status.java
@@ -4,36 +4,49 @@ import it.uniba.main.utilities.Strings;
 
 
 /** <<noECB>>
- * Enumerativo che contiene gli stati possibili.
+ * Enumerativo che rappresenta gli stati possibili
+ * e ne contiene una descrizione costituita da due parti.
  */
 
 public enum Status
 {
+    /**
+     * Status utilizzati durante la partita.
+     */
     partita_iniziata(Strings.INIZIO_PARTITA),
     partita_terminata(Strings.FINE_PARTITA);
-    
+
+    /**
+     * Stringhe contenenti le due parti della descrizione dello Status.
+     */
     private final String msg1;
     private String msg2;
 
+    /**
+     * Costruttore dell'enumerativo Status.
+     * @param msg1 prima parte della descrizione dello Status.
+     */
     private Status(String msg1)
     {
         this.msg1 = msg1;
         this.msg2 = "";
     }
-    private Status(String msg1, String msg2)
-    {
-        this.msg1 = msg1;
-        this.msg2 = msg2;
-    }
 
+    /**
+     * Imposta la seconda parte della descrizione dello Status.
+     * @param msg2 seconda parte della descrizione dello Status.
+     */
     void setMsg(String msg2)
     {
         this.msg2 = msg2;
     }
 
+    /**
+     * Restituisce la descrizione completa dello Status.
+     * @return concatenazione delle due parti della descrizione dello Status.
+     */
     public String getMsg()
     {
         return this.msg1 + this.msg2;
     }
-
 }
